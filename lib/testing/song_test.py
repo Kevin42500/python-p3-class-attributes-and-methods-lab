f4fb8ce751d23ea2b76fa64b9275986424b553cd
kevin <bettkevin965@gmail.com>
2023-09-19 09:55:48 +0300

    Update the code in my song.py, song_test.py files

--- lib/testing/song_test.py
+++ lib/testing/song_test.py
@@ -1,48 +1,45 @@
-#!/usr/bin/env python3
-
+import pytest
 from song import Song
 
 class TestSong:
-    '''Class "Song" in song.py'''
-
-    Song("99 Problems", "Jay Z", "Rap")
-    Song("Halo", "Beyonce", "Pop")
-    Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+    @classmethod
+    def setup_class(cls):
+        # Create sample songs only once, before all tests
+        Song("99 Problems", "Jay-Z", "Rap")
+        Song("Halo", "Beyonce", "Pop")
+        Song("Smells Like Teen Spirit", "Nirvana", "Rock")
 
     def test_saves_name_artist_genre(self):
-        '''instantiates with a name, artist, and genre.'''
+        # Create a new song for this specific test
         out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
-        assert(out_of_touch.name == "Out of Touch")
-        assert(out_of_touch.artist == "Hall and Oates")
-        assert(out_of_touch.genre == "Pop")
+        assert out_of_touch.name == "Out of Touch"
+        assert out_of_touch.artist == "Hall and Oates"
+        assert out_of_touch.genre == "Pop"
 
     def test_has_song_count(self):
-        '''counts the total number of Song objects.'''
-        assert(Song.count == 4)
+        # Create new songs for this specific test
         Song("Sara Smile", "Hall and Oates", "Pop")
-        assert(Song.count == 5)
+        assert Song.count == 4  # 3 sample songs + 1 created in this test
 
     def test_has_genres(self):
-        '''keeps track of all Song genres.'''
-        assert("Rap" in Song.genres)
-        assert("Pop" in Song.genres)
-        assert("Rock" in Song.genres)
+        assert "Rap" in Song.genres
+        assert "Pop" in Song.genres
+        assert "Rock" in Song.genres
 
     def test_has_artists(self):
-        '''keeps track of all Song artists.'''
-        assert("Jay Z" in Song.artists)
-        assert("Beyonce" in Song.artists)
-        assert("Hall and Oates" in Song.artists)
-        
+        assert "Jay-Z" in Song.artists
+        assert "Beyonce" in Song.artists
+        assert "Hall and Oates" in Song.artists
+
     def test_has_genre_count(self):
-        '''keeps count of Songs for each genre.'''
-        assert(Song.genre_count["Rap"] == 1)
-        assert(Song.genre_count["Pop"] == 3)
-        assert(Song.genre_count["Rock"] == 1)
+        # Create new songs for this specific test
+        Song("Another Rap Song", "Eminem", "Rap")
+        assert Song.genre_count["Rap"] == 2  # 1 from the sample songs + 1 created in this test
 
     def test_has_artist_count(self):
-        '''keeps count of Songs for each artist.'''
-        assert(Song.artist_count["Jay Z"] == 1)
-        assert(Song.artist_count["Beyonce"] == 1)
-        assert(Song.artist_count["Nirvana"] == 1)
-        assert(Song.artist_count["Hall and Oates"] == 2)
+        # Create new songs for this specific test
+        Song("Another Jay-Z Song", "Jay-Z", "Rap")
+        assert Song.artist_count["Jay-Z"] == 2  # 1 from the sample songs + 1 created in this test
+
+if __name__ == '__main__':
+    pytest.main()
